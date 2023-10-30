0af6d9af9c62e6d9bd6335f6b0d5cd2007b2626f
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-10-30 15:12:35 -0300

    aplicando autenticação ao login

--- frontend/src/hooks/useAuthentication.js
+++ frontend/src/hooks/useAuthentication.js
@@ -1,10 +1,10 @@
 import api from './api'
 import {useEffect,useState} from 'react'
-import {useNavegate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 export const useAuthentication = ()=>{
   const [authenticated, setAuthenticated] = useState('false')
-  const {navegate} = useNavegate()
+  const {navigate} = useNavigate()
 
   useEffect(()=>{
     const token = localStorage.getItem('token')
@@ -17,13 +17,13 @@ export const useAuthentication = ()=>{
   const authUser = (data)=>{
     setAuthenticated(true)
     localStorage.setItem('token',JSON.stringify(data.token))
-    navegate('/')
+    navigate('/')
   }
   const logout = ()=>{
     setAuthenticated(false)
     localStorage.removeItem('token')
     api.defaults.headers.Authorization = undefined
-    navegate('/')
+    navigate('/')
   }
 
   const authRegister = async(user)=>{
@@ -37,17 +37,23 @@ export const useAuthentication = ()=>{
 
     }
     
-
   }
 
   const authLogin = async(user)=>{
-    await api.post('/user/login',user)
-    .then((response)=>{
-      console.log(response.data);
-      sessionStorage.setItem('token',response.data.token)
-    }).catch(function (error){
-      console.log(error.response.data.errors)
-    })
+    try {
+      const data =await api.post('/user/login',user)
+      .then((response)=>{
+        console.log(response.data);
+        sessionStorage.setItem('token',response.data.token)
+      }).catch(function (error){
+        console.log(error.response.data.errors)
+      })
+      authUser(data)
+    } catch (error) {
+      
+    }
+    
+
   }
 
   return {authRegister,authLogin,logout,authenticated}
