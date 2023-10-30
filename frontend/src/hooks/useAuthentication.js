b3ba5ff229a91a41c94df61375d4863f6fc3e2f8
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-10-30 14:12:53 -0300

    autenticação de usuário

--- frontend/src/hooks/useAuthentication.js
+++ frontend/src/hooks/useAuthentication.js
@@ -1,16 +1,43 @@
 import api from './api'
 import {useEffect,useState} from 'react'
+import {useNavegate} from 'react-router-dom'
+
 export const useAuthentication = ()=>{
+  const [authenticated, setAuthenticated] = useState('false')
+  const {navegate} = useNavegate()
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    if(token){
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+      setAuthenticated(true)
+    }
+  },[])
+
+  const authUser = (data)=>{
+    setAuthenticated(true)
+    localStorage.setItem('token',JSON.stringify(data.token))
+    navegate('/')
+  }
+  const logout = ()=>{
+    setAuthenticated(false)
+    localStorage.removeItem('token')
+    api.defaults.headers.Authorization = undefined
+    navegate('/')
+  }
 
   const authRegister = async(user)=>{
-    await api.post('/user/register',user)
-    .then((response)=>{
-      console.log(response.data);
-      sessionStorage.setItem('token',response.data.token)
-    })
-    .catch(function (error) {
-      console.log(error.response.data.errors);
-    });
+    try{
+      const data = await api.post('/user/register',user)
+      .then((response)=>{
+        sessionStorage.setItem('token',response.data.token)
+      })
+      authUser(data)
+    }catch(error){
+
+    }
+    
+
   }
 
   const authLogin = async(user)=>{
@@ -23,5 +50,5 @@ export const useAuthentication = ()=>{
     })
   }
 
-  return {authRegister,authLogin}
+  return {authRegister,authLogin,logout,authenticated}
 }
