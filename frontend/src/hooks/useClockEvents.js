fbabfc5d3fb60bbe3c5adda651fed485e176525b
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-11-06 13:45:27 -0300

    Redirecionando após a deleção de tarefa

--- frontend/src/hooks/useClockEvents.js
+++ frontend/src/hooks/useClockEvents.js
@@ -1,5 +1,12 @@
 import api from './api'
+import { useNavigate } from 'react-router-dom'
+import { useAuthContext } from './useAuthContext'
+import { useTaskContext } from './useTaskContext'
 export const useClockEvents = () => {  
+
+    const navigate = useNavigate()
+    const {authenticated} = useAuthContext();
+    const {tasksRef} = useTaskContext()
 
     const token = localStorage.getItem('token')
 
@@ -8,9 +15,19 @@ export const useClockEvents = () => {
             headers:{
               authorization:`Bearer ${JSON.parse(token)}`
             }}
-        ).then().catch((err)=>{
+        ).then(()=>{
+            let newTasks = []
+            tasksRef.current.forEach(element => {
+                if(element._id !== id){
+                    newTasks.push(element)
+                }
+            });
+            tasksRef.current = newTasks
+            navigate('/')
+        }).catch((err)=>{
             console.log(err.response.data.errors)
         })
+
     }
     const editTaskById = async(id) =>{
         console.log('editar')
