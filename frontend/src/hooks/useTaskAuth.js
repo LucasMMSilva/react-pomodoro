aa7aec3b8486290657bec26115cf285ffaf9e96d
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-11-06 14:09:03 -0300

    correção do bug pós criação de tarefa

--- frontend/src/hooks/useTaskAuth.js
+++ frontend/src/hooks/useTaskAuth.js
@@ -2,8 +2,10 @@ import api from './api'
 import { useAuthContext } from './useAuthContext'
 import {useState,useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useTaskContext } from './useTaskContext'
 export const useTaskAuth = () => {
     const {authenticad,setAuthenticated} = useAuthContext()
+    const {tasksRef} = useTaskContext()
     const [loading,setLoading] = useState(true)
     const [token,setToken] = useState('')
     const navigate = useNavigate()
@@ -27,6 +29,7 @@ export const useTaskAuth = () => {
                   authorization:`Bearer ${JSON.parse(token)}`
                 }})
                 .then((response)=>{
+                    tasksRef.current.push(response.data)
                     navigate(`/task/${response.data._id}`)
                     
                 }).catch((err)=>{
